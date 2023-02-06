34cb2dbe1d26c362739824607760c743d58e3ace
Quy Phan <quyphan@Quys-MacBook-Pro-2.local>
2023-02-07 00:22:59 +0700

    hot test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-const Home: React.FC = ({ children }) => {
+const Home: React.FC = () => {
   const comments = useLoaderData();
 
   return <div className="text-red-500">{JSON.stringify(comments)}</div>;
